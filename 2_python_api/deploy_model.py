c2b9b0be82270f286571196b3f711dce9a30afde
Vicente Reyes-Puerta <vr.github@outlook.com>
2021-12-11 17:34:27 +0100

    Add predictions from dataframe; add formatting

--- 2_python_api/deploy_model.py
+++ 2_python_api/deploy_model.py
@@ -1,5 +1,17 @@
 from ludwig.api import LudwigModel
+import pandas as pd
 
 model = LudwigModel.load('./model/')
-predictions = model.predict('./reuters-allcats-head.csv')
-print(predictions)
+
+datafile_1 = './reuters-allcats-head.csv'
+predictions_1 = model.predict(
+    dataset=datafile_1, data_format="csv")
+print(predictions_1)
+
+dataframe_2 = pd.DataFrame({'text': [
+    'BAHIA COCOA REVIEW SALVADOR Feb 26 - Showers continued throughout the week',
+    'COBANCO INC ltCBCO YEAR NET SANTA CRUZ Calif Feb 26 - Shr 34 cts vs 119 dlrs'
+]})
+predictions_2 = model.predict(
+    dataset=dataframe_2, data_format='df')
+print(predictions_2)
