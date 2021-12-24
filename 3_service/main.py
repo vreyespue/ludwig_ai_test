c68d746f746cb1ded9cdcf04ff36791c3d23df9f
Vicente Reyes-Puerta <vr.github@outlook.com>
2021-12-24 16:11:05 +0100

    Process single review text through POST

--- 3_service/main.py
+++ 3_service/main.py
@@ -3,21 +3,22 @@ from pydantic import BaseModel
 from ludwig.api import LudwigModel
 import pandas as pd
 
-# poetry run uvicorn main:app --reload
-app = FastAPI()
-
 model = LudwigModel.load('./model')
-
-dataframe_2 = pd.DataFrame({'text': [
+dataframe_start = pd.DataFrame({'text': [
     'This was a really good movie, I had a lot of fun',
     'This was a very bad movie, it was really boring'
 ]})
-predictions_2 = model.predict(
-    dataset=dataframe_2, data_format='df')
-print(predictions_2)
+predictions_start = model.predict(
+    dataset=dataframe_start, data_format='df')
+# print(predictions_start)
 
-# class Review(BaseModel):
-#    text: str
+
+# poetry run uvicorn main:app --reload
+app = FastAPI()
+
+
+class Review(BaseModel):
+    text: str
 
 
 # curl http://0.0.0.0:8000/ -X GET
@@ -26,11 +27,15 @@ def read_root():
     return {"Hello": "World"}
 
 
-# @app.post("/predictions")
-# def update_item(review: Review):
-#     dataframe_1 = pd.DataFrame({'text': [review.text]})
-
-#     predictions_1 = model.predict(
-#         dataset=dataframe_1, data_format='df')
-#     print(predictions_1)
-#     return {predictions_1}
+# curl http://0.0.0.0:8000/predictions -X POST
+#     --data-raw '{"text": "Good movie"}' -H "Content-Type: application/json"
+@app.post("/predictions")
+def update_item(review: Review):
+    # print(review)
+    # print(review.text)
+    dataframe_1 = pd.DataFrame({'text': [review.text]})
+    predictions_1 = model.predict(
+        dataset=dataframe_1, data_format='df')
+    # print(predictions_1)
+    # print(list(predictions_1[0].label_predictions))
+    return list(predictions_1[0].label_predictions)
